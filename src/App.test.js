656c37ba5a8da043ff3cbcaeaddd3ed3dbcd0842
Planetoide <adcircus@gmail.com>
2021-02-17 21:49:31 -0600

    debe fallar, se agrego un test fallo

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('hay un link de google', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/google/i);
+  expect(linkElement).toBeInTheDocument();
+});
